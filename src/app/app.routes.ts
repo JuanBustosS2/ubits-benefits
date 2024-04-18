6159d7aa20c7aa2829a6900a4be94d4ecc3beaad
Johan Alvarez <yalvarez@ubits.co>
2024-04-18 13:15:29 -0500

    add empty list benefits page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingComponent } from './landing/landing.component';
 import { CreateBenefitComponent } from './create-benefit/create-benefit.component';
+import { ListBenefitsComponent } from './list-benefits/create-benefit.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'benefits/create', component: CreateBenefitComponent },
+  { path: 'benefits', component: ListBenefitsComponent },
 ];
--- src/app/list-benefits/create-benefit.component.ts
+++ src/app/list-benefits/create-benefit.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'landing',
   template: `
-    <h1>Welcome to list benefits!</h1>
+    <h1>Welcome to create benefit!</h1>
   `,
 })
-export class CreateBenefitComponent {}
+export class ListBenefitsComponent {}
