4945818a527f694ffb0f6129b3ae82ad6fc5a45b
Johan Alvarez <yalvarez@ubits.co>
2024-04-18 13:08:33 -0500

    new empty create benefits page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingComponent } from './landing/landing.component';
+import { CreateBenefitComponent } from './create-benefit/create-benefit.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'benefits/create', component: CreateBenefitComponent },
 ];
---
+++ src/app/create-benefit/create-benefit.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'landing',
+  template: `
+    <h1>Welcome to create benefit!</h1>
+  `,
+})
+export class CreateBenefitComponent {}
