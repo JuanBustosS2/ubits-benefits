0d8711d57b11f550976bca7ae4379b9bf0ef19fc
Johan Alvarez <yalvarez@ubits.co>
2024-04-18 13:39:29 -0500

    create benefits service

---
+++ src/app/services/benefits.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BenefitsService {
+    constructor(private firestore: Firestore) { }
+
+    createBenefit(benefit: any) {
+        return of(true);
+    }
+}
