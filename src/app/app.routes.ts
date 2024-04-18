5e69004c137ad87ad68f4be931227bc32dc268a9
Johan Alvarez <yalvarez@ubits.co>
2024-04-18 13:06:24 -0500

    add landing page and route for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: LandingComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
